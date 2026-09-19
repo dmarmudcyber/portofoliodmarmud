6438f3ded176ede1c7004430c354c1ec89cd5a06
dmarmudcyber <dmarmudcyber@gmail.com>
2026-09-19 14:59:24 +0700

    Add files via upload

---
+++ portofolio_dmarmud/redame.md
@@ -0,0 +1,8 @@
+Jelaskan cara install git
+Buka web git-scm.com lalu pilih install for windows
+lalu pilih yang X64 setup
+Tunggu hingga instalasi selesai
+Buka filenya, klik tombol next hingga install dan tunggu hingga finish
+
+Jelaskan cara login di github  
+Buka web github.com lalu pilih login menggunakan akun apa.
---
+++ portofolio_dmarmud/script.js
@@ -0,0 +1,8 @@
+// Menampilkan pesan di console browser untuk memastikan JS berjalan
+console.log("Selamat datang di Portofolio Saya!");
+console.log("Website ini dibuat menggunakan HTML, CSS, dan JS murni.");
+// Contoh interaksi sederhana: Memunculkan alert saat tombol diklik
+const tombolProyek = document.querySelector('.btn');
+tombolProyek.addEventListener('click', function() {
+console.log("Tombol 'Lihat Karya Saya' telah diklik!");
+});
